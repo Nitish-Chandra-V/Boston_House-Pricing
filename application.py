e32a0d110de7148d79dee1ee2f22ecf00d7a1fe1
Nitish <nitishchandra05@gmail.com>
2023-03-04 18:05:56 +0530

    updated

--- application.py
+++ application.py
@@ -5,15 +5,15 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
+application=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+@application.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
@@ -23,7 +23,7 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict',methods=['POST'])
+@application.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
@@ -34,5 +34,5 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    application.run(debug=True)
    
